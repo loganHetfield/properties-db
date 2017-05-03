bcbdc1577243e764c8f5b3e51d0b438141eb468d
James Rhodes <james.rhodes@esosolutions.com>
2017-05-03 16:54:50 -0500

    Fixing up .gitignore

--- build/modules/gitlib.psm1
+++
@@ -1,9 +0,0 @@
-function cleanGitSource
-{
-    [CmdletBinding()]  
-    param( $sourcePath )
-    git clean -df $sourcePath
-    if( !$? ){ exit 1 }
-    git checkout HEAD $sourcePath
-    if( !$? ){ exit 1 }
-}
