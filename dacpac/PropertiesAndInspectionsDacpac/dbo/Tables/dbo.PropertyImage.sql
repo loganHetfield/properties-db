f87c47aa026707f8aad58c22126a921dd34c2cb2
James Rhodes <james.rhodes@esosolutions.com>
2017-06-21 14:49:55 -0500

    Adding Property Image Table;

---
+++ dacpac/PropertiesAndInspectionsDacpac/dbo/Tables/dbo.PropertyImage.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PropertyImage] (
+    [PropertyImageId] UNIQUEIDENTIFIER NOT NULL,
+    [PropertyId]      UNIQUEIDENTIFIER NOT NULL,
+    [MimeType]        VARCHAR (64)     NOT NULL,
+    [FilePath]        NVARCHAR (256)   NOT NULL,
+    CONSTRAINT [PK_PropertyImage] PRIMARY KEY CLUSTERED ([PropertyImageId] ASC),
+    CONSTRAINT [FK_PropertyImage_Property] FOREIGN KEY ([PropertyId]) REFERENCES [dbo].[Property] ([PropertyId])
+);
+
