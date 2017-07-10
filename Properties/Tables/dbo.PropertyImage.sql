dd026a59834aecdc2dc54dd449e8b8bd5907b630
James Rhodes <james.rhodes@esosolutions.com>
2017-07-10 13:01:14 -0500

    PropertyImage -> Image

--- Properties/Tables/dbo.PropertyImage.sql
+++ Properties/Tables/dbo.PropertyImage.sql
@@ -1,13 +1,6 @@
-﻿IF NOT EXISTS (SELECT 1 FROM SYS.OBJECTS WHERE [NAME] = 'PropertyImage')
+﻿IF EXISTS (SELECT 1 FROM SYS.OBJECTS WHERE [NAME] = 'PropertyImage')
 BEGIN
 
-CREATE TABLE [dbo].[PropertyImage] (
-    [PropertyImageId] UNIQUEIDENTIFIER NOT NULL,
-    [PropertyId]      UNIQUEIDENTIFIER NOT NULL,
-    [MimeType]        VARCHAR (64)     NOT NULL,
-    [FilePath]        NVARCHAR (256)   NOT NULL,
-    CONSTRAINT [PK_PropertyImage] PRIMARY KEY CLUSTERED ([PropertyImageId] ASC),
-    CONSTRAINT [FK_PropertyImage_Property] FOREIGN KEY ([PropertyId]) REFERENCES [dbo].[Property] ([PropertyId])
-);
+DROP TABLE [dbo].[PropertyImage]);
 
 END
