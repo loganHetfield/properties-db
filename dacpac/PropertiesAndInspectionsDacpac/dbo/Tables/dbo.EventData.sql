df785a13a64d3e59efa194991cf4519797026986
James Rhodes <james.rhodes@esosolutions.com>
2017-07-25 18:11:45 -0500

    Removing DAC PAC

--- dacpac/PropertiesAndInspectionsDacpac/dbo/Tables/dbo.EventData.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[EventData] (
-    [EventDataId]    UNIQUEIDENTIFIER NOT NULL,
-    [EventId]        UNIQUEIDENTIFIER NOT NULL,
-    [EventDataKey]   NVARCHAR (50)    NOT NULL,
-    [EventDataValue] NVARCHAR (255)   NOT NULL,
-    CONSTRAINT [PK_EventData_1] PRIMARY KEY CLUSTERED ([EventDataId] ASC),
-    CONSTRAINT [FK_EventData_Event] FOREIGN KEY ([EventId]) REFERENCES [dbo].[Event] ([EventId])
-);
-
--- dacpac/PropertiesAndInspectionsDacpac/dbo/Tables/dbo.PropertyContactRole.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[PropertyContactRole] (
-    [PropertyContactRoleId] UNIQUEIDENTIFIER NOT NULL,
-    [PropertyContactId]     UNIQUEIDENTIFIER NOT NULL,
-    [PropertyId]            UNIQUEIDENTIFIER NOT NULL,
-    [RoleId]                INT              NULL,
-    CONSTRAINT [PK_PropertyContactRole] PRIMARY KEY CLUSTERED ([PropertyContactRoleId] ASC)
-);
-
