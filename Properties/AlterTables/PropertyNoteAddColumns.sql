a9c2767184d8d321e4f2d6a87a5261c395857a53
Brian Robbins <brian.robbins@esosolutions.com>
2017-08-01 11:31:31 -0500

    Making image fields consistant

--- Properties/AlterTables/PropertyNoteAddColumns.sql
+++ Properties/AlterTables/PropertyNoteAddColumns.sql
@@ -1,9 +1,14 @@
-if not exists( select 1 from sys.all_columns where object_id = object_id('PropertyNote') and (name = 'Width' or name = 'Height'))
+if not exists( select 1 from sys.all_columns where object_id = object_id('PropertyNote') and (name = 'ImageId' or name = 'ThumbImageId' or name = 'FileName'))
 BEGIN
 ALTER TABLE dbo.PropertyNote ADD
 	[ImageId]        UNIQUEIDENTIFIER NULL,
     [ThumbImageId]   UNIQUEIDENTIFIER NULL,
-    [FileName]       VARCHAR (255)    NULL,
-    [Width]          INT              NULL,
-    [Height]         INT              NULL
+    [FileName]       VARCHAR (255)    NULL
+END
+
+if not exists( select 1 from sys.all_columns where object_id = object_id('PropertyNote') and (name = 'ImageHeight' or name = 'ImageWidth'))
+BEGIN
+ALTER TABLE dbo.PropertyNote ADD
+    [ImageHeight]         INT              NULL,
+    [ImageWidth]          INT              NULL
 END
