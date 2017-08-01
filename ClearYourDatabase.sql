2c65887563081c9fa54d5221c9e9c22924338b5e
James Rhodes <james.rhodes@esosolutions.com>
2017-08-01 12:17:36 -0500

    Add a script to clear your databse

---
+++ ClearYourDatabase.sql
@@ -0,0 +1,9 @@
+delete Occupant
+delete Building
+delete PropertyContactRole
+delete PropertyContact
+Delete PropertyHydrant
+delete PropertyNote
+delete PropertyOtherWaterSource
+delete PropertySpecialHazard
+delete property
