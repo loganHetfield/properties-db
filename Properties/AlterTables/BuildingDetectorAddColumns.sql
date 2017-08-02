f8d95b35ead6f3ee6a6442272d8a4baaa349d98a
Victor Parra <vparra@intersysconsulting.com>
2017-08-02 17:27:38 -0500

    Renaming of fields in Building

--- Properties/AlterTables/BuildingDetectorAddColumns.sql
+++ Properties/AlterTables/BuildingDetectorAddColumns.sql
@@ -1,6 +1,6 @@
-if not exists( select 1 from sys.all_columns where object_id = object_id('BuildingAED') and (name = 'Width' or name = 'Height'))
+if not exists( select 1 from sys.all_columns where object_id = object_id('BuildingDetector') and (name = 'Width' or name = 'Height'))
 BEGIN
-ALTER TABLE dbo.BuildingAED ADD
+ALTER TABLE dbo.BuildingDetector ADD
 	  [ImageId]        UNIQUEIDENTIFIER NULL,
     [ThumbImageId]   UNIQUEIDENTIFIER NULL,
     [FileName]       VARCHAR (255)    NULL,
