cfda5761a83b3a70bfc8b46c6f0f9999c133c285
James Rhodes <james.rhodes@esosolutions.com>
2017-08-03 18:33:15 -0500

    Adding Missing Note Fields

--- Properties/AlterTables/PropertyNoteAddColumns.sql
+++ Properties/AlterTables/PropertyNoteAddColumns.sql
@@ -12,3 +12,18 @@ ALTER TABLE dbo.PropertyNote ADD
     [ImageHeight]         INT              NULL,
     [ImageWidth]          INT              NULL
 END
+
+if exists( select 1 from sys.all_columns where object_id = object_id('PropertyNote') and (name = 'Height' or name = 'Width'))
+BEGIN
+	ALTER TABLE dbo.PropertyNote Drop Column [Height]
+	ALTER TABLE dbo.PropertyNote Drop Column [Width]
+END
+
+if not exists( select 1 from sys.all_columns where object_id = object_id('PropertyNote') and name in ('CreatedBy','CreatedDate','UpdatedBy','UpdatedDate'))
+BEGIN
+ALTER TABLE dbo.PropertyNote ADD
+	CreatedBy nvarchar(50) NOT NULL,
+	CreatedDate datetime NOT NULL,
+	UpdatedBy nvarchar(50) NOT NULL,
+	UpdatedDate datetime NOT NULL
+END
