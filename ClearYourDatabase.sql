208fb0fad39887846c25112fe92caf05787b7e82
Brian Robbins <brian.robbins@esosolutions.com>
2017-08-04 11:19:30 -0500

    Added more items to the ClearYourDatabase script.
    	modified:   ClearYourDatabase.sql
    
    	modified:   ClearYourDatabase.sql

--- ClearYourDatabase.sql
+++ ClearYourDatabase.sql
@@ -1,9 +1,61 @@
-delete Occupant
-delete Building
-delete PropertyContactRole
-delete PropertyContact
-Delete PropertyHydrant
-delete PropertyNote
-delete PropertyOtherWaterSource
-delete PropertySpecialHazard
-delete property
+USE [PropertiesAndInspectionsV1]
+GO
+
+-- Attachement
+
+delete [Attachment]
+
+-- Event
+
+delete [EventData]
+delete [Event]
+
+-- Image
+
+delete [Image]
+
+-- Chemical & Tank
+
+delete [Tank]
+delete [Chemical]
+
+-- Occupant Document
+
+delete [OccupantAccess]
+delete [OccupantAED]
+delete [OccupantAlarm]
+delete [OccupantChemicalTank]
+delete [OccupantContact]
+delete [OccupantDetector]
+delete [OccupantExtinguisherSystem]
+delete [OccupantKnoxBox]
+delete [OccupantRiser]
+delete [OccupantShutOff]
+delete [OccupantSprinkler]
+delete [OccupantStandpipe]
+delete [Occupant]
+
+-- Building Document
+
+delete [BuildingAccess]
+delete [BuildingAED]
+delete [BuildingAlarm]
+delete [BuildingDetector]
+delete [BuildingExtinguisherSystem]
+delete [BuildingKnoxBox]
+delete [BuildingNote]
+delete [BuildingRiser]
+delete [BuildingShutOff]
+delete [BuildingSprinkler]
+delete [BuildingStandpipe]
+delete [Building]
+
+-- Properties Document
+
+delete [PropertyContactRole]
+delete [PropertyContact]
+delete [PropertyHydrant]
+delete [PropertyNote]
+delete [PropertyOtherWaterSource]
+delete [PropertySpecialHazard]
+delete [Property]
