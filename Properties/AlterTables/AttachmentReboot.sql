bd24553d3cdb12e19fa65944f1dd99842fe200ea
Brian Robbins <brian.robbins@esosolutions.com>
2017-08-14 16:15:44 -0500

    Added IsDeleted to Attachment table

--- Properties/AlterTables/AttachmentReboot.sql
+++ Properties/AlterTables/AttachmentReboot.sql
@@ -25,3 +25,9 @@ CREATE TABLE [dbo].[Attachment](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('Attachment') and (name = 'IsDeleted'))
+BEGIN
+ALTER TABLE dbo.[Attachment] ADD
+    [IsDeleted] BIT NOT NULL DEFAULT 0
+END
