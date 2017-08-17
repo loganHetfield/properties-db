34becad56819df7ffc9eb72c90f511331743ea44
Victor Parra <vparra@intersysconsulting.com>
2017-08-17 17:03:05 -0500

    Building Contact Read Model

---
+++ Properties/Tables/dbo.BuildingContactRole.sql
@@ -0,0 +1,12 @@
+﻿IF NOT EXISTS (SELECT 1 FROM SYS.OBJECTS WHERE [NAME] = 'BuildingContactRole')
+BEGIN
+
+CREATE TABLE [dbo].[BuildingContactRole] (
+    [BuildingContactRoleId] UNIQUEIDENTIFIER NOT NULL,
+    [BuildingContactId]     UNIQUEIDENTIFIER NOT NULL,
+    [BuildingId]            UNIQUEIDENTIFIER NOT NULL,
+    [RoleId]                INT              NULL,
+    CONSTRAINT [PK_BuildingContactRole] PRIMARY KEY CLUSTERED ([BuildingContactRoleId] ASC)
+);
+
+END
