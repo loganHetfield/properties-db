d77c84a5701fe65746181589678d70721a4b8162
James Rhodes <james.rhodes@esosolutions.com>
2017-08-28 12:25:26 -0500

    Doing away with the attachment reboot

--- Properties/RequireOrder/Attachment_1_AddCreateByLogin.sql
+++
@@ -1,8 +0,0 @@
-if not exists (select 1 from sys.all_columns where object_id = object_id('Attachment') and (name = 'CreatedByLogin'))
-BEGIN
-ALTER TABLE dbo.[Attachment] ADD
-    [CreatedByLogin] [nvarchar](256) NOT NULL
-	CONSTRAINT temp_login_defaut DEFAULT '',
-    [CreatedByFullName] [nvarchar](256) NOT NULL
-	CONSTRAINT temp_fullname_defaut DEFAULT ''
-END
--- Properties/Tables/dbo.Attachment.sql
+++ Properties/Tables/dbo.Attachment.sql
@@ -1,17 +1,29 @@
 ﻿IF NOT EXISTS (SELECT 1 FROM SYS.OBJECTS WHERE [NAME] = 'Attachment')
 BEGIN
 
-CREATE TABLE [dbo].[Attachment] (
-    [AttachmentId] UNIQUEIDENTIFIER NOT NULL,
-    [PropertyId]   UNIQUEIDENTIFIER NOT NULL,
-    [Name]         NVARCHAR (50)    NULL,
-    [Tag]          INT              NULL,
-    [UploadedBy]   UNIQUEIDENTIFIER NOT NULL,
-    [UploadedDate] DATETIME         NOT NULL,
-    [FileLocation] NVARCHAR (2048)  NOT NULL,
-    CONSTRAINT [PK_Attachment] PRIMARY KEY CLUSTERED ([AttachmentId] ASC),
-    CONSTRAINT [FK_Attachment_Property] FOREIGN KEY ([PropertyId]) REFERENCES [dbo].[Property] ([PropertyId])
-);
+CREATE TABLE [dbo].[Attachment](
+	[AttachmentId] [uniqueidentifier] NOT NULL,
+	[AttachmentType] [nvarchar](50) NOT NULL,
+	[PropertyId] [uniqueidentifier] NULL,
+	[BuildingId] [uniqueidentifier] NULL,
+	[OccupantId] [uniqueidentifier] NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[FileName] [nvarchar](1024) NOT NULL,
+	[ContentType] [nvarchar](50) NOT NULL,
+	[PrimaryId] [uniqueidentifier] NOT NULL,
+	[SecondaryId] [uniqueidentifier] NULL,
+	[ImageWidth] [int] NULL,
+	[ImageHeight] [int] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT ((0)),
+	[PrimaryFilePath] [nvarchar](1024) NOT NULL DEFAULT (''),
+	[SecondaryFilePath] [nvarchar](1024) NULL,
+	[CreatedByLogin] [nvarchar](256) NOT NULL,
+	[CreatedByFullName] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_Attachment] PRIMARY KEY CLUSTERED 
+(
+	[AttachmentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
 
 END
 
