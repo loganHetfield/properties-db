488d13544757f5ee78f6b644225adf239bd0cefb
Brian Robbins <brian.robbins@esosolutions.com>
2017-09-21 14:43:10 -0500

    New Fields

--- Properties/AlterTables/BuildingExtinguisherSystem.sql
+++ Properties/AlterTables/BuildingExtinguisherSystem.sql
@@ -1,4 +1,20 @@
-if not exists( select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and name in ('ImageId', 'ThumbImageId', 'FileName', 'Width', 'Height'))
+if exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and name = 'ExtinguishingSystem')
+BEGIN
+	EXECUTE sp_rename '[dbo].[BuildingExtinguisherSystem].[ExtinguishingSystem]', 'ExtinguishingSystemId', 'Column'
+END
+
+if exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and name = 'TestType')
+BEGIN
+	EXECUTE sp_rename '[dbo].[BuildingExtinguisherSystem].[TestType]', 'TestTypeId', 'Column'
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and name = 'ExtinguisherServiceDate')
+BEGIN
+ALTER TABLE [dbo].BuildingExtinguisherSystem Add 
+	[ExtinguisherServiceDate] DATE NULL
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and name in ('ImageId', 'ThumbImageId', 'FileName', 'Width', 'Height'))
 BEGIN
 ALTER TABLE dbo.BuildingExtinguisherSystem ADD
     [ImageId]        UNIQUEIDENTIFIER NULL,
@@ -8,14 +24,20 @@ ALTER TABLE dbo.BuildingExtinguisherSystem ADD
     [Height]         INT              NULL
 END
 
-if not exists( select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and (name = 'ImageHeight' or name = 'ImageWidth'))
+if not exists (select 1 from sys.all_columns where object_id = object_id('Building') and name = 'TotalValue')
+BEGIN
+ALTER TABLE [dbo].[Building] Add 
+	[TotalValue] INT NULL
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and (name = 'ImageHeight' or name = 'ImageWidth'))
 BEGIN
 ALTER TABLE dbo.BuildingExtinguisherSystem ADD
     [ImageHeight]         INT              NULL,
     [ImageWidth]          INT              NULL
 END
 
-if exists( select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and (name = 'Height' or name = 'Width'))
+if exists (select 1 from sys.all_columns where object_id = object_id('BuildingExtinguisherSystem') and (name = 'Height' or name = 'Width'))
 BEGIN
     ALTER TABLE dbo.BuildingExtinguisherSystem Drop Column [Height]
     ALTER TABLE dbo.BuildingExtinguisherSystem Drop Column [Width]
