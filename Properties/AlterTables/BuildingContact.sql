9ef7e70c65425edb0bc096c073e9d224b5ec2ead
Brian Robbins <brian.robbins@esosolutions.com>
2017-09-22 15:17:21 -0500

    New Feilds

---
+++ Properties/AlterTables/BuildingContact.sql
@@ -0,0 +1,19 @@
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingContact') and name = 'FaxNumber')
+BEGIN
+    ALTER TABLE [dbo].[BuildingContact] ADD [FaxNumber] [NVARCHAR](50) NULL
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingContact') and name = 'Website')
+BEGIN
+    ALTER TABLE [dbo].[BuildingContact] ADD [Website] [NVARCHAR](255) NULL
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingContact') and name = 'BusinessName')
+BEGIN
+    ALTER TABLE [dbo].[BuildingContact] ADD [BusinessName] [NVARCHAR](50) NULL
+END
+
+if not exists (select 1 from sys.all_columns where object_id = object_id('BuildingContact') and name = 'IsNameSameAsProperty')
+BEGIN
+    ALTER TABLE [dbo].[BuildingContact] ADD [IsNameSameAsProperty] [BIT] NULL
+END
--- Properties/AlterTables/PropertyContact.sql
+++ Properties/AlterTables/PropertyContact.sql
@@ -22,17 +22,37 @@ EXEC sp_rename '[dbo].[PropertyContact].[Width]', 'ImageWidth', 'Column';
 
 END
 
-if not exists (select 1 from sys.all_columns where object_id = object_id('PropertyContact') and (name = 'FaxNumber'))
+
+if exists( select 1 from sys.all_columns where object_id = object_id('PropertyContact') and (name = 'WebSite'))
 BEGIN
-ALTER TABLE dbo.PropertyContact ADD
-	[FaxNumber] [nvarchar](255) NULL,
-	[WebSite] [nvarchar](255) NULL,
-	[BusinessName] [nvarchar](255) NULL,
-	[IsNameSameAsProperty] [bit] NULL
-END 
+
+EXEC sp_rename '[dbo].[PropertyContact].[WebSite]', 'Website', 'Column';
+
+END
 
 if not exists (select 1 from sys.all_columns where object_id = object_id('PropertyContact') and (name = 'RoomNumber'))
 BEGIN
 ALTER TABLE dbo.PropertyContact ADD
 	[RoomNumber] nvarchar(255) NULL
 END 
+
+if exists ( select name, is_nullable from sys.all_columns where object_id = object_id('PropertyContact') and name in ('NumberOrMilepost', 'StreetOrHighway', 'City', 'StateId', 'Zip') and Is_nullable = 0)
+BEGIN
+Alter table PropertyContact
+Alter column 
+	[NumberOrMilepost] [nvarchar](50) NULL
+Alter table PropertyContact
+Alter column 
+	[StreetOrHighway] [nvarchar](100) NULL
+Alter table PropertyContact
+Alter column 
+	[City] [nvarchar](255) NULL
+Alter table PropertyContact
+Alter column 
+	[StateId] [int] NULL
+Alter table PropertyContact
+Alter column 
+	[Zip] [nvarchar](50) NULL
+END
+
+	
